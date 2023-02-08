06a6bcae5927148075799aff0b96421ef98a48c3
kawakami <barcarolle.op60.horowitz@gmail.com>
2023-02-08 22:30:39 +0900

    メモ編集のルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/store', 'HomeController@store')->name('store');
 Route::get('/edit/{id}', 'HomeController@edit')->name('edit');
+Route::post('/update', 'HomeController@update')->name('update');
