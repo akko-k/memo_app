fac026ee2c515e0732fd59065df46cb20dfaf772
kawakami <barcarolle.op60.horowitz@gmail.com>
2023-02-12 20:47:02 +0900

    メモ削除のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/store', 'HomeController@store')->name('store');
 Route::get('/edit/{id}', 'HomeController@edit')->name('edit');
 Route::post('/update', 'HomeController@update')->name('update');
+Route::post('/destroy', 'HomeController@destroy')->name('destroy');
