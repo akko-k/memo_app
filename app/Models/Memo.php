986b13d285e95f0e9813b05cca49cc4dac8e89ff
kawakami <barcarolle.op60.horowitz@gmail.com>
2023-03-26 15:01:50 +0900

    コメント追加

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Memo extends Model
 {
+    // ユーザーを指定
     public function scopeUser($query)
     {
-        return $query->where('user_id', \Auth::id());
+        return $query->where('user_id', Auth::id());
     }
 
+    // 有効なものを指定
     public function scopeActive($query)
     {
         return $query->whereNull('deleted_at');
